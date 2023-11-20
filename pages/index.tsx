e1cb68cfb1577ea6fafe4be168f49c5ab2ada7c7
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-11-20 07:16:16 +0700

    feat: add experience, testimonials, help components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,19 @@
-import { customFetch } from '@/utils/fetchApi'
-import { GetStaticProps } from 'next'
+import Carousel from '@/components/landing/Carousel'
+import Company from '@/components/landing/Company'
+import Experience from '@/components/landing/Experience'
+import Help from '@/components/landing/Help'
+import PopularProperties from '@/components/landing/PopularProperties'
+import Testimonials from '@/components/landing/Testimonials'
 
-const urlSale =
-  '/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6'
-
-const urlRent =
-  '/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6'
-
-type Product = {
-  id: number
-  title: string
-  // Tambahkan tipe data untuk atribut produk lainnya di sini
-}
-
-type HomeProps = {
-  products: Product[]
-}
-
-export const getStaticProps: GetStaticProps = async () => {
-  const response = await customFetch(urlSale)
-  console.log(response.data.hits)
-
-  const products: Product[] = response.data.hits
-
-  return {
-    props: {
-      products,
-    },
-  }
-}
-
-export default function Home({ products }: HomeProps) {
+export default function Home() {
   return (
     <>
-      {products.map((product) => (
-        <div key={product.id}>
-          <h2>{product.title}</h2>
-        </div>
-      ))}
+      <Carousel />
+      <Company />
+      <PopularProperties />
+      <Experience />
+      <Testimonials />
+      <Help />
     </>
   )
 }
